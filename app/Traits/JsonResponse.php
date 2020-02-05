e585847f97b094cf78507423863f22db05eca2c6
Ari Putra <ariteknologi@gmail.com>
2020-02-06 03:42:39 +0700

    add base helper

---
+++ app/Traits/JsonResponse.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Traits;
+
+trait JsonResponse {
+
+    public function responseWithCondition($data, $successMsg, $failMsg)
+    {
+        return $data ? $this->success($successMsg, $data)
+            : $this->fail($failMsg);
+    }
+
+    public function success($msg, $data = null, $code = 200, $version = '1.0')
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $msg,
+            'data' => $data,
+            'code' => $code,
+            'version' => $version
+        ], $code);
+    }
+
+    public function fail($msg, $code = 400, $version = '1.0')
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $msg,
+            'data' => null,
+            'code' => $code,
+            'version' => $version
+        ], $code);
+    }
+
+    public function remoteSuccessResponse($response)
+    {
+        return response($response, json_decode($response)->code)->header('Content-Type', 'application/json');
+    }
+
+    public function remoteFailResponse($response)
+    {
+        return response($response, json_decode($response)->code)->header('Content-Type', 'application/json');
+    }
+}
