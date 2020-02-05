7638f1c3a69873e92f56825afebfd200dd8c511e
Ari Putra <ariteknologi@gmail.com>
2020-02-06 03:12:39 +0700

    add cause model n migration

---
+++ app/Models/Cause.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cause extends Model
+{
+    //declare table name
+    protected $table = 'causes';
+
+    //declare field able to fill
+    protected $fillable = [
+        'name'
+    ];
+
+    public function getNameAttribute($value)
+    {
+        return ucwords($value);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+    }
+}
